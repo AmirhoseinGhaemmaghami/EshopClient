9c6193d6d5ec331e4ffcc84c60e51a10e33fe040
amirhosein <amirhosein.ghaemmaghami@gmail.com>
2023-01-02 18:33:35 +0330

    contact us and about us added with routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,18 @@ const routes: Routes = [
     loadChildren: () =>
       import('./home/home.module').then((mod) => mod.HomeModule),
   },
+  {
+    path: 'about-us',
+    loadChildren: () =>
+      import('./about-us/about-us.module').then((mod) => mod.AboutUsModule),
+  },
+  {
+    path: 'contact-us',
+    loadChildren: () =>
+      import('./contact-us/contact-us.module').then(
+        (mod) => mod.ContactUsModule
+      ),
+  },
 ];
 
 @NgModule({

1a5d71c5628621b9ad28fdf766092fd494e1154a
amirhosein <amirhosein.ghaemmaghami@gmail.com>
2023-01-02 16:45:38 +0330

    slider calling script from custom.js

--- src/app/home/Components/slider/slider.component.ts
+++ src/app/home/Components/slider/slider.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Slider } from '../../Models/Slider';
 import { SliderService } from '../../services/slider.service';
+
+declare function homeSlider(): any;
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements OnInit, AfterViewInit {
   constructor(private sliderService: SliderService) {}
+  ngAfterViewInit(): void {
+    setInterval(() => homeSlider(), 1000);
+  }
 
   sliders$: Observable<Slider[]> = new Observable<Slider[]>();
 
